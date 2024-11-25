66dd3461ed746b770908554dc1f45aa7859ac4a8
Sudo-Self <sudo-self@proton.me>
2024-11-24 17:38:12 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -14,7 +13,6 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-
 export const metadata = {
   title: "JesseJesse.com",
   description: "Home is where the heart is...",
@@ -24,8 +22,37 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-     
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>JesseJesse.com</title>
+        <meta name="title" content="JesseJesse.com" />
+        <meta
+          name="description"
+          content="A Three JS Rendering of the spot with included playlist"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jessejesse.com" />
+        <meta property="og:title" content="JesseJesse.com" />
+        <meta
+          property="og:description"
+          content="A Three JS Rendering of the spot with included playlist"
+        />
+        <meta
+          property="og:image"
+          content="https://jessejesse.com/jessejesse.jpeg"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://jessejesse.com" />
+        <meta property="twitter:title" content="JesseJesse.com" />
+        <meta
+          property="twitter:description"
+          content="A Three JS Rendering of the spot with included playlist"
+        />
+        <meta
+          property="twitter:image"
+          content="https://jessejesse.com/jessejesse.jpeg"
+        />
+        <meta name="author" content="Jesse Roper" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -39,5 +66,6 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
 
 
